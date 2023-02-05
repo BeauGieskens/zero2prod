8a07a029049f957f6f985e3fb39222d5146450a2
Beau Gieskens <beau.gieskens@gmail.com>
2023-02-05 18:22:15 +1000

    Done with chapter 3
    I don't like the idea of spamming new databases for testing because I don't like depending on Docker

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,16 @@
 use std::net::TcpListener;
 
-use zero2prod::{
-    config::{get_configuration, Settings},
-    startup::run,
-};
+use sqlx::PgPool;
+use zero2prod::{config::get_configuration, startup::run};
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
     let configuration = get_configuration().expect("Failed to read configuration");
+    let db_pool = PgPool::connect(&configuration.database.connection_string())
+        .await
+        .expect("Failed to connect to Postgres.");
     let address = format!("localhost:{}", configuration.application_port);
 
     let listener = TcpListener::bind(address).expect("Failed to bind address");
-    run(listener)?.await
+    run(listener, db_pool)?.await
 }
